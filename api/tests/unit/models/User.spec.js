974be67badedbc7fb4075fe6513c199d8ec37b95
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-11 17:03:39 +0100

    refined register test

--- api/tests/unit/models/User.spec.js
+++ api/tests/unit/models/User.spec.js
@@ -63,7 +63,11 @@ describe('User', () => {
                 .mockResolvedValueOnce({rows:{}})
                 .mockResolvedValueOnce({rows: [userTwo]})
             const result = await User.register(userTwo.first_name, userTwo.surname, userTwo.username, userTwo.password)
+            expect(result).toHaveProperty('id', '2');
             expect(result).toHaveProperty('username', 'fpmitnick');
+            expect(result).toHaveProperty('first_name', 'futureproof');
+            expect(result).toHaveProperty('surname', 'mitnick');
+            expect(result).toHaveProperty('password', 'password');          
         })
 
         test('user already exists', async () => {
