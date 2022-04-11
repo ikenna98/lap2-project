e8c56240e0a3be599f4ef186e150c931e1ba392b
Peter Ly <peter.ly1@hotmail.com>
2022-04-12 00:01:04 +0100

    Made improvements on html css and js

--- client/login.js
+++ client/login.js
@@ -2,7 +2,6 @@ const login = document.querySelector('.login-box')
 const register = document.querySelector('.register-btn');
 const newAccount = document.querySelector('.new-account');
 const close = document.querySelector('.popup-close');
-
 const username = document.getElementById('username');
 const password = document.querySelector('.password');
 const firstName = document.querySelector('#first-name');
@@ -11,32 +10,10 @@ const habit = document.querySelector('.habit');
 
 register.addEventListener('click', (e) => {
     e.preventDefault()
-    newAccount.style.display = 'block'
+    newAccount.style.display = 'block';
+
 });
 
 close.addEventListener('click', () => {
-    newAccount.style.display = 'none'
+    newAccount.style.display = 'none';
 })
-
-
-const addHabits = document.querySelector('.add-habit'); //Selecting the form
-const habitsList = document.querySelector('.habits')//Selecting the habit list
-const habits = []; //where we will add the habit list to
-
-//add habit
-function addHabit(e) {
-    e.preventDefault();
-    const text = this.querySelector("[name=habit]").value;
-    console.log(text)
-
-}
-
-//List the habit
-
-//Toggle if complete
-
-//Delete Habit
-
-
-
-addHabits.addEventListener('submit', addHabit);
